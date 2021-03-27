1db7427901fd2d9ac5e8cad1a5e573baa44ec989
Genivaldo Silva <genivaldo.silva.mar@gmail.com>
2021-03-27 00:18:22 -0300

    Novos progressos

--- relogio_ponto/lib/views/relogio.dart
+++ relogio_ponto/lib/views/relogio.dart
@@ -10,12 +10,31 @@ class _RelogioState extends State<Relogio> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.only(top: 100),
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Voltar para Home'),
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  final snackBar = SnackBar(content: Text("Fui tocado!"));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                child: Image(
+                  image: AssetImage('lib/assets/clock.png'),
+                  width: 200,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Text('Toque no relógio para registrar o horário'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Voltar para Home'),
+              ),
+            ],
           ),
         ),
       ),
